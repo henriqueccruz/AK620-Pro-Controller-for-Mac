5b9dc14cf50d4939ef4a8de6feabe5fc2c11b52b
Henrique Cruz <eu@henriquecruz.com.br>
2025-02-07 16:37:57 -0300

    Initial Commit

---
+++ DeepCool/DeepCoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepCoolApp.swift
+//  DeepCool
+//
+//  Created by Henrique Cruz on 07/02/25.
+//
+
+import SwiftUI
+
+@main
+struct DeepCoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeepCoolTests/DeepCoolTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeepCoolTests.swift
+//  DeepCoolTests
+//
+//  Created by Henrique Cruz on 07/02/25.
+//
+
+import Testing
+@testable import DeepCool
+
+struct DeepCoolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
